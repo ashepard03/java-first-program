d820cd27ed7aa87c69ad28aed7da57b5a206ce72
ashepard03 <ashepard@appdev.yearup.org>
2023-04-08 20:36:55 -0400

    Completed module

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -8,7 +8,7 @@ public class Finance {
     public static final String BEST_LOAN_RATES = "bestLoanRates";
     public static final String SAVINGS_CALCULATOR = "savingsCalculator";
     public static final String MORTGAGE_CALCULATOR = "mortgageCalculator";
-    public static final Map<String, String> commandsToUsage = Map.of(BEST_LOAN_RATES, "usage:bestLoanRates",
+    public static final Map<String, String> commandsToUsage = Map.of(BEST_LOAN_RATES, "usage: bestLoanRates",
             SAVINGS_CALCULATOR, "usage: savingsCalculator <credits separated by ','> <debits separated by ','>",
             MORTGAGE_CALCULATOR, "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>");
     private static boolean validateCommandArguments(String [] args) {
@@ -21,7 +21,7 @@ public class Finance {
     }
     private static void executeCommand(String command, String[] arguments) {
         switch (command){
-            case BEST_LOAN_RATES: System.out.println("Finding the best loan rates ..."); BestLoanRates.main(arguments);
+            case BEST_LOAN_RATES: System.out.println("Finding best loan rates ..."); BestLoanRates.main(arguments);
             return;
             case SAVINGS_CALCULATOR: System.out.println("Finding your net savings ..."); SavingsCalculator.main(arguments);
             return;
@@ -32,16 +32,16 @@ public class Finance {
 
     public static void main(String[] args){
          String command = args[0];
-         if (!commandsToUsage.containsKey(command)){
-            System.out.println(":command not found");
+         if (!commandsToUsage.containsKey(command)) {
+            System.out.println(command + ":command not found");
             return;
         }
          boolean isValidCommand = validateCommandArguments(args);
-         if (!isValidCommand){
-             System.out.println(commandsToUsage.get(command));
+         if (!isValidCommand) {
+             System.out.println(commandsToUsage.get(args[0]));
              return;
          }
-         executeCommand(command, Arrays.copyOf(args, 1));
+         executeCommand(command, Arrays.copyOfRange(args, 1, args.length));
     }
 
 }
