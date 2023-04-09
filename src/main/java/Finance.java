94b054f94320eca64e81fd29345118e055990619
ashepard03 <ashepard@appdev.yearup.org>
2023-04-08 20:43:21 -0400

    Completed module

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -21,11 +21,17 @@ public class Finance {
     }
     private static void executeCommand(String command, String[] arguments) {
         switch (command){
-            case BEST_LOAN_RATES: System.out.println("Finding best loan rates ..."); BestLoanRates.main(arguments);
+            case BEST_LOAN_RATES:
+                System.out.println("Finding best loan rates ...");
+                BestLoanRates.main(arguments);
             return;
-            case SAVINGS_CALCULATOR: System.out.println("Finding your net savings ..."); SavingsCalculator.main(arguments);
+            case SAVINGS_CALCULATOR:
+                System.out.println("Finding your net savings ...");
+                SavingsCalculator.main(arguments);
             return;
-            case MORTGAGE_CALCULATOR: System.out.println("Finding your monthly payment ..."); MortgageCalculator.main(arguments);
+            case MORTGAGE_CALCULATOR:
+                System.out.println("Finding your monthly payment ...");
+                MortgageCalculator.main(arguments);
             return;
         }
     }
@@ -41,6 +47,7 @@ public class Finance {
              System.out.println(commandsToUsage.get(args[0]));
              return;
          }
+
          executeCommand(command, Arrays.copyOfRange(args, 1, args.length));
     }
 
